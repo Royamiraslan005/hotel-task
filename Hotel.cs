38a207c62350fa693cbc42889aa4c7ec8d9f315c
Royamiraslan005 <royaca-ab109@code.edu.az>
2025-04-02 23:02:07 +0400

    hotelllll

---
+++ Hotel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Hotel
+    {
+        public string Name { get; set; }
+        private Room[] Rooms;
+
+        public Hotel(string ad)
+        {
+            Name=Name;
+            Rooms= new Room[0];
+        }
+
+        public void AddRoom(Room room)
+        {
+            {
+                Array.Resize(ref Rooms, Rooms.Length + 1);
+                Rooms[Rooms.Length - 1] = room;
+                Console.WriteLine($"Otaq {room.Id} elave olundu!");
+            }
+
+
+        public void RezervEt(int roomID)
+        {
+            bool tapildi = false;
+            foreach (Otaq otaq in Otaqlar)
+            {
+                if (otaq.ID == otaqID)
+                {
+                    tapildi = true;
+                    if (otaq.RezerveEdileBiler)
+                    {
+                        otaq.RezerveEdileBiler = false;
+                        Console.WriteLine($"Otaq {otaqID} uğurla rezerv edildi.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bağışlayın, bu otaq artıq doludur.");
+                    }
+                    break;
+                }
+            }
+
+            if (!tapildi)
+            {
+                Console.WriteLine("Səhv ID daxil etdiniz, belə bir otaq mövcud deyil!");
+            }
+        }
+
+        public void ButunOtaqlariGoster()
+        {
+            if (Otaqlar.Length == 0)
+            {
+                Console.WriteLine("Hazırda hoteldə heç bir otaq yoxdur.");
+                return;
+            }
+
+            foreach (Otaq otaq in Otaqlar)
+            {
+                otaq.MelumatGoster();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using ConsoleApp20;
+
+Hotel hotel = new Hotel("Green City Hotel");
+
+while (true)
+{
+    Console.WriteLine("\n1. Create a room");
+    Console.WriteLine("2. Add room");
+    Console.WriteLine("3. Make reservation");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.Write("Enter room name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter price: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter person capacity: ");
+            int capacity = Convert.ToInt32(Console.ReadLine());
+
+            Room newRoom = new Room(name, price, capacity);
+            Console.WriteLine($"Otaq {newRoom.Id} elave olundu!");
+            break;
+
+        case "2":
+            if (newRoom != null)
+            {
+                hotel.AddRoom(newRoom);
+                Console.WriteLine($"Room {newRoom.Id} added to the hotel!");
+            }
+            else
+            {
+                Console.WriteLine("No room created yet!");
+            }
+            break;
+
+        case "3":
+            Console.Write("Enter room ID to reserve: ");
+            int roomId = Convert.ToInt32(Console.ReadLine());
+            hotel.MakeReservation(roomId);
+            break;
+
+        case "0":
+            Console.WriteLine("Exiting program...");
+            return;
+
+        default:
+            Console.WriteLine("Invalid choice! Try again.");
+            break;
+    }
+}
+    
